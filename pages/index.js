d987ad0b6d2c3630129716e5fcd9278512298307
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2022-11-27 23:26:21 +0600

    created routes

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
     <Head>
-      <title>Swagstoar</title>
+      <title>Swagstoar - free swag!</title>
       <meta name="description" content="Swagstoar.vercel.app - wear code t-shirts and more" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
---
+++ pages/product/[slug].js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+const Slug = () => {
+    const router = useRouter()
+    const {slug} = router.query
+  return (
+    <div>
+      the slug is: {slug}
+    </div>
+  )
+}
+
+export default Slug
