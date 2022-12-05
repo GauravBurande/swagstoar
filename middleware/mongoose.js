64d0ac51dc0c91c86a9766ae8d8569eedfa47cc0
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2022-12-05 14:15:14 +0600

    added server side props to populate tshirt page with response data

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -2,11 +2,9 @@ import mongoose from "mongoose";
 
 const connectDb = handler => (req, res) => {
     if (mongoose.connections[0].readyState) {
-        console.log('mongo already connected')
         return handler(req, res)
     }
     mongoose.connect(process.env.MONGO_URI)
-    console.log('mongo connected')
     return handler(req, res);
 }
 
--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,113 +1,45 @@
 import React from 'react'
 import Link from 'next/link'
+import Product from "../models/Product"
+import mongoose from "mongoose";
 
-const Tshirts = () => {
+const Tshirts = ({ products }) => {
   return (
     <div>
       <section className="text-gray-400 bg-gray-900 body-font">
         <div className="container px-5 py-12 mx-auto">
           <div className="flex flex-wrap -m-4">
-              <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-2xl">
-            <Link href={'/product/docker-kill'}>
-                <a className="block static h-55 rounded overflow-hidden">
-                  <img alt="tshirt" className="object-cover object-center w-full h-full block" src="https://cdn.shopify.com/s/files/1/0437/7057/7061/products/40fde45bbac3dd55ea69ddc14a2152bc_1024x1024@2x.jpg?v=1597721564" />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
-                  <h2 className="text-white title-font text-lg font-medium">Docker Kill - Unisex Jersey Short Sleeve Tee</h2>
-                  <p className="mt-1">$18.99</p>
-                </div>
-            </Link>
-              </div>
-              <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-2xl">
-            <Link href={'/product/docker-kill'}>
-                <a className="block static h-55 rounded overflow-hidden">
-                  <img alt="tshirt" className="object-cover object-center w-full h-full block" src="https://cdn.shopify.com/s/files/1/0437/7057/7061/products/40fde45bbac3dd55ea69ddc14a2152bc_1024x1024@2x.jpg?v=1597721564" />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
-                  <h2 className="text-white title-font text-lg font-medium">Docker Kill - Unisex Jersey Short Sleeve Tee</h2>
-                  <p className="mt-1">$18.99</p>
-                </div>
-            </Link>
-              </div>
-              <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-2xl">
-            <Link href={'/product/docker-kill'}>
-                <a className="block static h-55 rounded overflow-hidden">
-                  <img alt="tshirt" className="object-cover object-center w-full h-full block" src="https://cdn.shopify.com/s/files/1/0437/7057/7061/products/40fde45bbac3dd55ea69ddc14a2152bc_1024x1024@2x.jpg?v=1597721564" />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
-                  <h2 className="text-white title-font text-lg font-medium">Docker Kill - Unisex Jersey Short Sleeve Tee</h2>
-                  <p className="mt-1">$18.99</p>
-                </div>
-            </Link>
-              </div>
-              <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-2xl">
-            <Link href={'/product/docker-kill'}>
-                <a className="block static h-55 rounded overflow-hidden">
-                  <img alt="tshirt" className="object-cover object-center w-full h-full block" src="https://cdn.shopify.com/s/files/1/0437/7057/7061/products/40fde45bbac3dd55ea69ddc14a2152bc_1024x1024@2x.jpg?v=1597721564" />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
-                  <h2 className="text-white title-font text-lg font-medium">Docker Kill - Unisex Jersey Short Sleeve Tee</h2>
-                  <p className="mt-1">$18.99</p>
-                </div>
-            </Link>
-              </div>
-              <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-2xl">
-            <Link href={'/product/docker-kill'}>
-                <a className="block static h-55 rounded overflow-hidden">
-                  <img alt="tshirt" className="object-cover object-center w-full h-full block" src="https://cdn.shopify.com/s/files/1/0437/7057/7061/products/40fde45bbac3dd55ea69ddc14a2152bc_1024x1024@2x.jpg?v=1597721564" />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
-                  <h2 className="text-white title-font text-lg font-medium">Docker Kill - Unisex Jersey Short Sleeve Tee</h2>
-                  <p className="mt-1">$18.99</p>
-                </div>
-            </Link>
-              </div>
-              <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-2xl">
-            <Link href={'/product/docker-kill'}>
-                <a className="block static h-55 rounded overflow-hidden">
-                  <img alt="tshirt" className="object-cover object-center w-full h-full block" src="https://cdn.shopify.com/s/files/1/0437/7057/7061/products/40fde45bbac3dd55ea69ddc14a2152bc_1024x1024@2x.jpg?v=1597721564" />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
-                  <h2 className="text-white title-font text-lg font-medium">Docker Kill - Unisex Jersey Short Sleeve Tee</h2>
-                  <p className="mt-1">$18.99</p>
-                </div>
-            </Link>
-              </div>
-              <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-2xl">
-            <Link href={'/product/docker-kill'}>
-                <a className="block static h-55 rounded overflow-hidden">
-                  <img alt="tshirt" className="object-cover object-center w-full h-full block" src="https://cdn.shopify.com/s/files/1/0437/7057/7061/products/40fde45bbac3dd55ea69ddc14a2152bc_1024x1024@2x.jpg?v=1597721564" />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
-                  <h2 className="text-white title-font text-lg font-medium">Docker Kill - Unisex Jersey Short Sleeve Tee</h2>
-                  <p className="mt-1">$18.99</p>
-                </div>
-            </Link>
-              </div>
-              <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-2xl">
-            <Link href={'/product/docker-kill'}>
-                <a className="block static h-55 rounded overflow-hidden">
-                  <img alt="tshirt" className="object-cover object-center w-full h-full block" src="https://cdn.shopify.com/s/files/1/0437/7057/7061/products/40fde45bbac3dd55ea69ddc14a2152bc_1024x1024@2x.jpg?v=1597721564" />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
-                  <h2 className="text-white title-font text-lg font-medium">Docker Kill - Unisex Jersey Short Sleeve Tee</h2>
-                  <p className="mt-1">$18.99</p>
-                </div>
-            </Link>
-              </div>
+            {products.map((item) => {
+              return (
+                <div key={item._id} className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-2xl">
+                  <Link href={`/product/${item.slug}`}>
+                    <div className="block static h-55 rounded overflow-hidden">
+                      <img alt="tshirt" className="object-cover object-center w-full h-full block" src={item.image} />
+                    </div>
+                    <div className="mt-4">
+                      <h3 className="text-gray-500 text-xs text tracking-widest title-font mb-1">{item.category}</h3>
+                      <h2 className="text-white title-font text-lg font-medium">{item.title}</h2>
+                      <p className="mt-1">${item.price}.00</p>
+                    </div>
+                  </Link>
+                </div>)
+            })}
           </div>
         </div>
       </section>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  if (!mongoose.connections[0].readyState) {
+    mongoose.connect(process.env.MONGO_URI)
+  }
+
+  let products = await (Product.find({ category: 'tshirt' }))
+  return {
+    props: { products: JSON.parse(JSON.stringify(products)) }
+  }
 }
 
 export default Tshirts
