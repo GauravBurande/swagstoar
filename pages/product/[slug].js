b9efb98225f0fabf43cccdb6bb4659d060bdca83
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-01-04 09:06:34 +0530

    modified slug page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -70,7 +70,7 @@ const Post = ({ addToCart, toggleCart, product, buyNow }) => {
         />
         <div className="container px-5 py-24 mx-auto">
           <div className="lg:w-4/5 mx-auto flex flex-wrap">
-            <img alt={product.title} className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src={product.image} />
+            <img alt={product.title} className="lg:w-1/2 w-full h-auto object-cover object-center rounded" src={product.image} />
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
               <h2 className="text-sm title-font text-gray-500 tracking-widest">SWAGSTOAR</h2>
               <h1 className="text-white text-3xl title-font font-medium mb-1">{product.title}</h1>
